aabbd68c9479d04caef15dbc636cd668b1ebaff8
Yuya Takashina <takashina2051@gmail.com>
2020-11-07 22:54:16 +0900

    Removed OptSeries class and converted its methods to functions

--- optpanda/optseries.py
+++ optpanda/optseries.py
@@ -1,42 +1,57 @@
+import numpy as np
 import pandas as pd
 from pandas import Series
 from pandas.core.dtypes.common import is_list_like
 
-from optpanda.backend import variable, dtype
+import optpanda.backend as B
 
 
-# このクラス自体をインスタンス化しないのであれば関数で良い。やるなら pandas.Series 拡張したい。
-# OptSeries に対して最適化関係のメソッドを実装することを考えると、やはりこの方向性がよさそう。
-# - 境界値の設定
-# - 区分線形近似
-# - etc.
-class OptSeries:
-    __data: Series
+def from_index(name, index: pd.Index, lb=None, ub=None, cat="Continuous", **kwargs):
+    if not isinstance(index, pd.Index):
+        raise ValueError("index must be a pandas.Index.")
 
-    def __init__(self, data: Series = None):
-        self.__data = data
+    if not isinstance(lb, np.number):
+        if not isinstance(lb, pd.Series):
+            raise ValueError("lb must be a Series if not a scalar.")
+        if not index.equals(lb.index):
+            raise ValueError("lb must have the same index with index if it is a Series.")
 
-    @classmethod
-    def create(cls, name, index=None, fix=None, lb=None, ub=None, cat="Continuous", **kwargs):
-        # TODO: lb, ub が index と同じ shape の場合
-        # TODO: fix を実装。Gurobi などは変数宣言に時間がかかるため、無駄な変数宣言をしないように。
-        # TODO: 特に MultiIndex になる場合に sortorder 等の設定で高速化できないか調査
+    if not isinstance(ub, np.number):
+        if not isinstance(ub, pd.Series):
+            raise ValueError("ub must be a Series if not a scalar.")
+        if not index.equals(ub.index):
+            raise ValueError("ub must have the same index with index if it is a Series.")
 
-        if index is None:
-            return variable(name, lb, ub, cat, **kwargs)
-        if isinstance(index, pd.Index):
-            values = [variable(f"{name}[{i}]", lb, ub, cat, **kwargs) for i in index]
-            return OptSeries(pd.Series(values, index, name=name, dtype=dtype()))
+    values = [B.variable(f"{name}[{i}]", lb, ub, cat, **kwargs) for i in index]
+    return pd.Series(values, index, name=name, dtype=B.dtype())
 
-        if all(is_list_like(v) for v in index):
-            index = pd.MultiIndex.from_product(index)
-            return OptSeries.create(name, index=index, lb=lb, ub=ub, cat="cat", **kwargs)
-        elif is_list_like(index):
-            index = pd.Index(index)
-            return OptSeries.create(name, index=index, lb=lb, ub=ub, cat="cat", **kwargs)
-        else:
-            raise ValueError(f"{type(index)} is not supported for ``index``.")
 
-    def fix(self, data: Series = None):
-        """Set mask"""
-        raise NotImplementedError()
+def create(name, index, fix=None, lb=None, ub=None, cat="Continuous", **kwargs):
+    # TODO: fix を実装。Gurobi などは変数宣言に時間がかかるため、無駄な変数宣言をしないように。
+    # TODO: 特に MultiIndex になる場合に sortorder 等の設定で高速化できないか調査
+    if fix is not None:
+        raise Warning("``fix`` has not been implemented yet. This argument will be ignored.")
+    if np.ndim(index) > 2:
+        raise ValueError("``index`` dimension cannot be greater than 2.")
+
+    if isinstance(index, pd.Index):
+        pass
+    elif all(is_list_like(v) for v in index):
+        index = pd.MultiIndex.from_product(index)
+    elif is_list_like(index):
+        index = pd.Index(index)
+    else:
+        raise ValueError(f"{type(index)} is not supported for ``index``.")
+    return from_index(name, index, lb, ub, cat, **kwargs)
+
+
+def fix(varseries: Series, values: Series):
+    raise NotImplementedError()
+
+
+def set_border(varseries: Series, index):
+    raise NotImplementedError()
+
+
+def approx_piecewise():
+    raise NotImplementedError()
